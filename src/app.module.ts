e484ae0838271e5fcd59991eb17a4aa82feb418d
Yevhwnii <mirandoos3223@gmail.com>
2020-11-23 19:16:53 +0100

    injecting auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     RestaurantsModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
   ],
+  exports: [ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
